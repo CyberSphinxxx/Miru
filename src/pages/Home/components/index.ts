75ac7a81bf6fd21688bd8712cf4f07220fd408d1
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-03-26 20:36:47 +0800

    Refactor: modularize src/pages/Home/components/index.ts

---
+++ src/pages/Home/components/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Home Components - Barrel Export
+ */
+
+export { default as HeroSection } from './HeroSection';
+export type { HeroSectionProps } from './HeroSection';
+
+export { default as ContinueWatchingRow } from './ContinueWatchingRow';
+
+export { default as AnimeRow } from './AnimeRow';
+export type { AnimeRowProps } from './AnimeRow';
